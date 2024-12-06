d769a395d4e44358b31a884de988523530d49dea
zhangjianqiao <zhangjianqiao0403@gmail.com>
2024-12-06 08:05:11 +0800

    采用vant3开发tabbar

--- src/components/tab-bar.vue
+++ src/components/tab-bar.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="tab-bar">
     
+
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
     <template v-for="(item, index) in tabbarData" :key="index">
-      <div class="tab-bar-item" :class="{active: currentIndex === index}" @click="itemClick(index, item)">
-        <img :src="getAssetURL(item.imgUrl)" alt=""  v-if="index !== currentIndex">
-        <img :src="getAssetURL(item.imgActiveUrl)" alt=""  v-else>
-        <span class="text">{{item.text}}</span>
-      </div>
+      <van-tabbar-item :to="item.path">
+        <span>{{ item.text }}</span>
+        <template #icon="props">
+          <img :src="getAssetURL(item.imgUrl)" alt=""  v-if="index !== currentIndex">
+          <img :src="getAssetURL(item.imgActiveUrl)" alt=""  v-else>
+        </template>
+      </van-tabbar-item>
     </template>
+    
+    
+    
+</van-tabbar>
+
   </div>
 </template>
 
@@ -15,44 +24,15 @@
 import { tabbarData } from '@/assets/data/tabbar';
 import { getAssetURL } from '@/utils/load_asset';
 import { ref } from 'vue';
-import { useRouter } from 'vue-router';
 
-const router = useRouter()
 const currentIndex = ref(0)
-const itemClick = (index, item) => {
-  currentIndex.value = index
-  router.push(item.path)
-}
+
 </script>
 
 <style scoped lang="less">
   .tab-bar {
-    position: fixed;
-    display: flex;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    height: 50px;
-    border-top: 1px solid #f3f3f3;
-    
-    .tab-bar-item {
-      display: flex;
-      flex: 1;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-      
-      &.active {
-        color: var(--primary-color);
-      }
-      img {
-        width: 36px;
-      }
-      .text {
-        font-size: 12px;
-        margin-top: 2px;
-      }
+    img {
+      height: 25px;
     }
-
   }
 </style>
